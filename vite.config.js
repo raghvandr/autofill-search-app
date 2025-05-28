d272b42fb172ec58f8cd6e6512a5a3f148a4da4a
raghvendra Upadhyay <78319113+raghvandr@users.noreply.github.com>
2025-05-28 11:15:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 80,      // Change port to 80
+    host: true,     // Allow access via localhost and network IP
+    allowedHosts: ['face-frontend-7hyl.onrender.com']
+  }
 })
